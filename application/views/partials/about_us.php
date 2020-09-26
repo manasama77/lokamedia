45fde88d76d7324e9b49e32a5e55e0ad5c01d6f1
Adam PM <adam.pm77@gmail.com>
2020-09-26 12:40:04 +0700

    perbaikan about us

--- application/views/partials/about_us.php
+++ application/views/partials/about_us.php
@@ -3,12 +3,12 @@
 		<!-- Featured Project Row-->
 		<div class="row align-items-center no-gutters">
 			<div class="col-xl-6 col-lg-7">
-				<img class="img-fluid mb-3 mb-lg-0" src="<?=base_url();?>assets/img/<?=$about_us->row()->photo;?>" alt="Foto Founder" />
+				<img class="img-fluid mb-3 mb-lg-0" src="<?= base_url(); ?>assets/img/about_us/<?= $about_us->row()->photo; ?>" alt="Foto Founder" />
 			</div>
 			<div class="col-xl-6 col-lg-5">
 				<div class="featured-text text-center text-lg-left">
-					<h4 class="text-dark"><?=$about_us->row()->title;?></h4>
-					<p class="text-dark mb-0"><?=$about_us->row()->content;?></p>
+					<h4 class="text-dark"><?= $about_us->row()->title; ?></h4>
+					<p class="text-dark mb-0"><?= $about_us->row()->content; ?></p>
 				</div>
 			</div>
 		</div>
